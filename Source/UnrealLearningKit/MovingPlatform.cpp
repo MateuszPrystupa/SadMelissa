1631cf3f625461d651cc038e9ed0253bf9948697
Szwagrowicz <mateusz.prystupa@gmail.com>
2024-07-31 21:54:36 +0200

    Initial commit

---
+++ Source/UnrealLearningKit/MovingPlatform.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	StartLocation = GetActorLocation();
+
+
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	RotatePlatform(DeltaTime);
+	ShouldPlatformReturn();
+	MovePlatform(DeltaTime);
+}
+
+void AMovingPlatform::MovePlatform(float DeltaTime)
+{	
+	if (ShouldPlatformReturn())
+	{
+		FVector MoveDirection = MovingPlatform.GetSafeNormal();
+		StartLocation = StartLocation + MoveDirection * DistanceMoved;
+
+		MovingPlatform = -MovingPlatform;
+	}
+	else
+	{
+		FVector CurrentLocation = GetActorLocation();
+		CurrentLocation += MovingPlatform * DeltaTime;
+		SetActorLocation(CurrentLocation);
+	}
+}
+
+void AMovingPlatform::RotatePlatform(float DeltaTime)
+{
+	UE_LOG(LogTemp, Display, TEXT("Rotation platform %s"), *GetName());
+}
+
+bool AMovingPlatform::ShouldPlatformReturn()
+{
+	
+	return GetDistanceMoved() > DistanceMoved;
+}
+
+float AMovingPlatform::GetDistanceMoved()
+{
+	return FVector::Dist(StartLocation, GetActorLocation());
+
+}
